7a25039bfea0f493433a4bfd6f50988c5c6176f4
pitow <pitowka@gmail.com>
2023-12-03 17:23:22 +0100

    2023 Day 1

---
+++ src/pitowka/aoc2023/day1/Day1_1.java
@@ -0,0 +1,98 @@
+package pitowka.aoc2023.day1;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public class Day1_1 {
+
+    public static void main(String[] args) throws IOException {
+        System.err.println(
+            new Scanner(Objects.requireNonNull(Day1_1.class.getResourceAsStream("/day1.txt")))
+                .tokens()
+                    .map(NumberFromString::new)
+                    .mapToInt(NumberFromString::number)
+                    .sum()
+        );
+
+        System.err.println(
+            new Scanner(Objects.requireNonNull(Day1_1.class.getResourceAsStream("/day1.txt")))
+                .tokens()
+                .map(NumberFromText::new)
+                .mapToInt(NumberFromText::number)
+                .sum()
+        );
+    }
+
+    public static class NumberFromString {
+        private static final Pattern FIRST_NUMBER = Pattern.compile("^\\D*(\\d).*");
+        private static final Pattern LAST_NUMBER = Pattern.compile("^.*(\\d)\\D*$");
+        private final String s;
+
+        public NumberFromString(String s) {
+            this.s = s;
+        }
+
+        public Integer number(){
+            return Integer.valueOf(
+                "%s%s".formatted(number(FIRST_NUMBER), number(LAST_NUMBER))
+            );
+        }
+
+        private String number(Pattern p){
+            return p
+                .matcher(s)
+                .results()
+                .map(m -> m.group(1))
+                .findFirst()
+                .orElse(null);
+        }
+    }
+
+    public static class NumberFromText {
+        private static final String NUMBERS = "\\d|one|two|three|four|five|six|seven|eight|nine";
+        private static final Pattern FIRST_NUMBER = Pattern.compile("^.*?("+NUMBERS+").*");
+        private static final Pattern LAST_NUMBER = Pattern.compile(".*("+NUMBERS+").*?$");
+
+        private static final Map<String, Integer> MAPPING = new HashMap<>(Map.of(
+                "one", 1,
+                "two", 2,
+                "three", 3,
+                "four", 4,
+                "five", 5,
+                "six", 6,
+                "seven", 7,
+                "eight", 8,
+                "nine", 9
+
+        ));
+        private final String s;
+
+        public NumberFromText(String s) {
+            this.s = s;
+        }
+
+        public Integer number(){
+            return Integer.valueOf(
+                "%s%s".formatted(
+                    MAPPING.computeIfAbsent(number(FIRST_NUMBER), Integer::valueOf),
+                    MAPPING.computeIfAbsent(number(LAST_NUMBER), Integer::valueOf)
+                )
+            );
+        }
+
+        private String number(Pattern p){
+            return p
+                    .matcher(s)
+                    .results()
+                    .map(m -> m.group(1))
+                    .findFirst()
+                    .orElse(null);
+        }
+    }
+}
