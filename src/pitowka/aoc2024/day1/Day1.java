d608e4d9bf286a5b25e137b0e262cfcdb67614de
pitowka <pitowka@gmail.com>
2024-12-05 00:04:07 +0100

    Day1 of 2024

--- src/pitowka/aoc2024/day1/Day1.java
+++ src/pitowka/aoc2024/day1/Day1.java
@@ -1,4 +1,76 @@
 package pitowka.aoc2024.day1;
 
+import pitowka.aoc.InputFileLines;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Day1 {
+    public static void main(String[] args){
+
+        System.err.println(
+            new Pairs(
+                new InputFileLines("/aoc2024/day1.txt")
+                    .lines()
+                    .map(Pair::new)
+                    .toList()
+            ).distance()
+        );
+
+        System.err.println(
+            new Pairs(
+                new InputFileLines("/aoc2024/day1.txt")
+                    .lines()
+                    .map(Pair::new)
+                    .toList()
+            ).similarity()
+        );
+    }
+
+    record Pair(Integer left, Integer right){
+        Pair(String line){
+            this(
+            Integer.valueOf(line.split("\\s+")[0]),
+            Integer.valueOf(line.split("\\s+")[1]));
+        }
+    }
+
+    record Pairs(List<Pair> pairs){
+        private List<Integer> left(){
+            return sortedSide(Pair::left);
+        }
+
+        private List<Integer> right(){
+            return sortedSide(Pair::right);
+        }
+
+        private Map<Integer, Long> rightQuantity(){
+            return right().stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        }
+
+        private List<Integer> sortedSide(Function<? super Pair, Integer> sideFunction){
+            return pairs.stream()
+                    .map(sideFunction)
+                    .sorted()
+                    .toList();
+        }
+
+        Integer distance() {
+            int distance = 0;
+            for(int i = 0; i < pairs.size(); i++){
+                distance += Math.abs(left().get(i) - right().get(i));
+            }
+
+            return distance;
+        }
+
+        Long similarity(){
+            return left().stream()
+                    .mapToLong(l -> l * rightQuantity().getOrDefault (l, 0L))
+                    .sum();
+        }
+    }
 }
